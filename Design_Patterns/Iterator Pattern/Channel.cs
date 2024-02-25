0398f90f0a8a000ada6ac33d50bf228bad9b92df
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-25 15:19:04 +0600

    Iterator Pattern Radio Channel Example

---
+++ Design_Patterns/Iterator Pattern/Channel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Iterator_Pattern
+{
+    public class Channel
+    {
+        public double Frequency { get; private set; }
+        public string Name { get; private set; }
+        public Channel(double frequency, string name)
+        {
+            Frequency = frequency;
+            Name = name;
+        }
+    }
+}
