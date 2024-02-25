82f7c3921af73a9904c0d0095e768c355bac23e2
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-25 13:59:13 +0600

    Facade Pattern Email validation Example

---
+++ Design_Patterns/Facade Pattern/CustomerDataAccessLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Facade_Pattern
+{
+    public class CustomerDataAccessLayer
+    {
+        public bool SaveCustomer(Customer customer)
+        {
+            Console.WriteLine("\nCustomer Saved into the Database...");
+            return true;
+        }
+    }
+}
