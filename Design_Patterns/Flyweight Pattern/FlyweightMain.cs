d82e34eb430aeba3d4cc90b121d9ca91311cb722
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-25 11:58:40 +0600

    Flyweight Pattern with Forrest Example

---
+++ Design_Patterns/Flyweight Pattern/FlyweightMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Flyweight_Pattern
+{
+    public class FlyweightMain
+    {
+        public static void Main()
+        {
+            var forest = new List<Tree>();
+            forest.Add(new Tree(1, 2, TreeFactory.GetTreeType("Pine", "Green", "PineTexture")));
+            forest.Add(new Tree(10, 20, TreeFactory.GetTreeType("Pine", "Green", "PineTexture")));
+            forest.Add(new Tree(3, 4, TreeFactory.GetTreeType("Oak", "Brown", "OakTexture")));
+            forest.Add(new Tree(5, 7, TreeFactory.GetTreeType("Oak", "Brown", "OakTexture")));
+            forest.Add(new Tree(7, 9, TreeFactory.GetTreeType("Oak", "Brown", "OakTexture")));
+            forest.Add(new Tree(12, 14, TreeFactory.GetTreeType("Oak", "Brown", "OakTexture")));
+            forest.Add(new Tree(15, 19, TreeFactory.GetTreeType("Oak", "Brown", "OakTexture")));
+            foreach (var tree in forest)
+            {
+                tree.Draw();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design_Patterns/Flyweight Pattern/TreeFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Flyweight_Pattern
+{
+    public class TreeFactory
+    {
+        private static readonly Dictionary<string, TreeType> _treeTypes = new Dictionary<string, TreeType>();
+        public static TreeType GetTreeType(string name, string color, string texture)
+        {
+            var key = $"{name}_{color}_{texture}";
+            if (!_treeTypes.ContainsKey(key))
+            {
+                var type = new TreeType(name, color, texture);
+                _treeTypes.Add(key, type);
+            }
+            return _treeTypes[key];
+        }
+    }
+}
