890c8dda134c633462390365fc8af46bd4a0049b
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-25 16:54:01 +0600

    Strategy Pattern Travel Example

---
+++ Design_Patterns/Strategy Pattern/AutoTravelStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Strategy_Pattern
+{
+    public class AutoTravelStrategy : ITravelStrategy
+    {
+        public void GotoDestination()
+        {
+            Console.WriteLine("Traveler going to the destination by Auto at 60tk per km");
+        }
+    }
+}
---
+++ Design_Patterns/Strategy Pattern/BusTravelStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Strategy_Pattern
+{
+    public class BusTravelStrategy : ITravelStrategy
+    {
+        public void GotoDestination()
+        {
+            Console.WriteLine("Traveler going to the destination by Bus at 15tk per km");
+        }
+    }
+}
