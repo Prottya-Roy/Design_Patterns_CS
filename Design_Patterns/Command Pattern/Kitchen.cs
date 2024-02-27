298d5bc4e79cfa9382a10c382745688c1699706a
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-27 15:01:39 +0600

    Command Pattern with Kitchen Order example

---
+++ Design_Patterns/Command Pattern/Kitchen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Command_Pattern
+{
+    public class Kitchen
+    {
+        public void PrepareFish()
+        {
+            Console.WriteLine("Preparing Fish as per Order...");
+        }
+
+        public void PrepareBurger()
+        {
+            Console.WriteLine("Preparing Burger as per Order...");
+        }
+
+        public void PreparePasta()
+        {
+            Console.WriteLine("Preparing Pasta as per Order...");
+        }
+
+        public void PrepareMeat()
+        {
+            Console.WriteLine("Preparing Meat as per Order...");
+        }
+    }
+}
---
+++ Design_Patterns/Command Pattern/Waiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Command_Pattern
+{
+    public class Waiter 
+    {
+        private List<IOrderCommand> orders = new List<IOrderCommand>();
+
+        public void TakeOrder(IOrderCommand order)
+        {
+            orders.Add(order);
+        }
+
+        public void CompleteOrder()
+        {
+            foreach (IOrderCommand order in orders)
+            {
+                order.Execute();
+            }
+            orders.Clear();
+        }
+    }
+}
