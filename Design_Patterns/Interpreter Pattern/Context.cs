0e563636c54fa078cd5582be1a512287cbfc1e4f
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-27 14:26:14 +0600

    Interpreter Pattern with Roman number example

---
+++ Design_Patterns/Interpreter Pattern/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Interpreter_Pattern
+{
+    public class Context
+    {
+        public string Input { get; set; }
+        public int Output { get; set; }
+    }
+}
---
+++ Design_Patterns/Interpreter Pattern/ThousandExpression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Interpreter_Pattern
+{
+    public class ThousandExpression : AExpression
+    {
+        public override string One() => "M";
+        public override string Four() => " ";
+        public override string Five() => " ";
+        public override string Nine() => " ";
+        public override int Multiplier() => 1000;
+    }
+}
