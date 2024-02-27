cdb33e3b28767646eb26f477d2c782f625339241
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-27 15:23:27 +0600

    Memento Pattern Bank Transaction Example

---
+++ Design_Patterns/Memento Pattern/TransactionHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Memento_Pattern
+{
+    public class TransactionHistory
+    {
+        private Stack<TransactionMemento> _transactions = new Stack<TransactionMemento>();
+
+        public void SaveState(BankAccount account)
+        {
+            _transactions.Push(account.CreateMemento());
+        }
+        public TransactionMemento UndoTransaction()
+        {
+            return _transactions.Pop();
+        }
+    }
+}
