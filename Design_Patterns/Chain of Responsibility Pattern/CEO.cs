9c2469840b0f98016d198d779b82aa98a498e82d
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-28 15:15:01 +0600

    CoR with Expense Approval Example

---
+++ Design_Patterns/Chain of Responsibility Pattern/CEO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Chain_of_Responsibility_Pattern
+{
+    public class CEO : AapprovalHandler
+    {
+        public override void ApproveRequest(Expense expense)
+        {
+            if (expense.amount >=10000)
+            {
+                Console.WriteLine($"CEO Approved Expense of ${expense.amount}");
+            }
+            else
+            {
+                Console.WriteLine("Amount not allowed to be approved...");
+            }
+        }
+    }
+}
---
+++ Design_Patterns/Chain of Responsibility Pattern/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Chain_of_Responsibility_Pattern
+{
+    public class Expense
+    {
+        public double amount {  get; set; }
+        public Expense(double amount)
+        {
+            this.amount = amount;
+        }
+    }
+}
