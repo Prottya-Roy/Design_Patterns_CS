38093cf5010586d0cd3bcf6c810ad19616297f1a
Prottya Roy Chayan <prottya.roy@orbitax.com>
2024-02-28 15:31:05 +0600

    Mediator Pattern Airplane Control Tower Example

---
+++ Design_Patterns/Mediator Pattern/MediatorMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Mediator_Pattern
+{
+    public class MediatorMain
+    {
+        public static void Main(string[] args)
+        {
+            IControlTower controlTower = new ControlTower();
+            
+            controlTower.RegisterRunway(new Runway("R1"));
+            var airplane1 = new Airplane("FL123", controlTower);
+            var airplane2 = new Airplane("FL456", controlTower);
+            airplane1.RequestLanding();
+            airplane2.RequestLanding();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design_Patterns/Mediator Pattern/Runway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Mediator_Pattern
+{
+    public class Runway
+    {
+        public string Id { get; }
+        public Runway(string id)
+        {
+            Id = id;
+        }
+    }
+}
